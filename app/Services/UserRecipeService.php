573938eb8411584160b8bddf350fa657fdfecb56
Janko Blagojevic <janko.tbbt@gmail.com>
2025-04-24 23:32:03 +0200

    recipe stuff

--- app/Services/UserRecipeService.php
+++ app/Services/UserRecipeService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Http\Controllers\UserController;
+use App\Models\FoodstuffCategory;
+use App\Models\Recipe;
 use App\Repositories\UserRecipeRepository;
 
 class UserRecipeService
@@ -20,13 +22,32 @@ class UserRecipeService
     public function getUserRecipesByDate($userId, $startDate, $endDate) {
         $recipes = $this->userRecipeRepository->getUserRecipes($userId, $startDate, $endDate);
         foreach ($recipes as &$recipe) {
+            $r = Recipe::where('id', $recipe->recipe_id)->first();
             $recipe->foodstuffs = $recipe->foodstuffs;
+            $recipe->type = $r->type;
+            $description = str_replace('\n', "\n", $r->description);
+            $recipe->steps = preg_split('/\r\n|\r|\n/', $description);
+            $recipe->steps = array_filter($recipe->steps, fn($step) => trim($step) !== '');
+            $cal = 0;
+            $prot = 0;
+            $fat = 0;
+            $ch = 0;
+            foreach ($recipe->foodstuffs as &$foodstuff) {
+                $cal += $foodstuff->amount * ($foodstuff->foodstuff->calories / 100);
+                $prot += $foodstuff->amount * ($foodstuff->foodstuff->proteins / 100);
+                $fat += $foodstuff->amount * ($foodstuff->foodstuff->fats / 100);
+                $ch += $foodstuff->amount * ($foodstuff->foodstuff->carbohydrates / 100);
+                $foodstuff->foodstuff_category = FoodstuffCategory::where('id', $foodstuff->foodstuff->foodstuff_category_id)->get()[0]->name;
+                $foodstuff->name = $foodstuff->foodstuff->name;
+            }
+
+            $recipe->calAmount = $cal;
+            $recipe->proteinAmount = $prot;
+            $recipe->fatsAmount = $fat;
+            $recipe->chAmount = $ch;
         }
-        $recipesByDate = $recipes->groupBy(function ($recipe) {
-            return \Carbon\Carbon::parse($recipe->date)->format('Y-m-d');
-        });
-        $recipesByDate = $recipesByDate->sortKeys();
-        return $recipesByDate;
+
+        return $recipes;
     }
 
 }
