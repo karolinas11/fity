5253d688f939acd8b337576aae6696a76cddf14c
Janko Blagojevic <janko.tbbt@gmail.com>
2025-05-20 22:58:24 +0200

    featured image

--- app/Services/UserRecipeService.php
+++ app/Services/UserRecipeService.php
@@ -30,6 +30,7 @@ class UserRecipeService
             $recipe->type = $r->type;
             $recipe->name = $r->name;
             $recipe->id = $r->id;
+            $recipe->featured_image = $r->featured_image;
             $description = str_replace('\n', "\n", $r->description);
             $recipe->steps = preg_split('/\r\n|\r|\n/', $description);
             $recipe->steps = array_filter($recipe->steps, fn($step) => trim($step) !== '');
