822f5e8f85bdcf3d649f712802f1115621f13852
Janko Blagojevic <janko.tbbt@gmail.com>
2025-06-15 11:53:04 +0200

    user water

--- app/Repositories/UserWaterRepository.php
+++ app/Repositories/UserWaterRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\UserWater;
+use Carbon\Carbon;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Log;
 
@@ -11,12 +12,18 @@ class UserWaterRepository
     public function updateUserWater($userId, $water)
     {
         try {
+            $today = Carbon::today()->format('Y-m-d');
             $userWater = UserWater::where('user_id', $userId)
+                ->where('date', $today)
                 ->get()
                 ->first();
-            $userWater->water += $water;
-            $userWater->save();
-            return $userWater;
+            if($userWater == null) {
+                return UserWater::create(['user_id' => $userId, 'water' => $water, 'date' => $today]);
+            } else {
+                $userWater->water += $water;
+                $userWater->save();
+                return $userWater;
+            }
         } catch (QueryException $e) {
             Log::error('Can\'t update water: ' . $e->getMessage());
         }
