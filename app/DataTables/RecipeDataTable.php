11b56cdfae2aadf5eb9ae1bb8855344ad2205936
Janko Blagojevic <janko.tbbt@gmail.com>
2025-06-16 15:23:13 +0200

    filters and columns

--- app/DataTables/RecipeDataTable.php
+++ app/DataTables/RecipeDataTable.php
@@ -2,7 +2,9 @@
 
 namespace App\DataTables;
 
+use App\Models\Foodstuff;
 use App\Models\Recipe;
+use App\Services\RecipeFoodstuffService;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
@@ -14,6 +16,48 @@ use Yajra\DataTables\Services\DataTable;
 
 class RecipeDataTable extends DataTable
 {
+
+    protected RecipeFoodstuffService $recipeFoodstuffService;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->recipeFoodstuffService = new RecipeFoodstuffService();
+    }
+
+    public function getRecipeData(Recipe $recipe) {
+        $recipeFoodstuffs = $this->recipeFoodstuffService->getRecipeFoodstuffs($recipe->id);
+        $fats = 0;
+        $proteins = 0;
+        $carbs = 0;
+        foreach( $recipeFoodstuffs as $recipeFoodstuff){
+            $foodstuff = Foodstuff::find($recipeFoodstuff->foodstuff_id);
+            $proteins += ($recipeFoodstuff->amount / 100) * $foodstuff->proteins;
+            $fats += ($recipeFoodstuff->amount / 100) * $foodstuff->fats;
+            $carbs += ($recipeFoodstuff->amount / 100) * $foodstuff->carbohydrates;
+        }
+
+        $totalMass = $proteins + $fats + $carbs;
+
+        $proteinPercentage = $totalMass > 0 ? ($proteins / $totalMass) * 100 : 0;
+        $fatPercentage = $totalMass > 0 ? ($fats / $totalMass) * 100 : 0;
+        $carbPercentage = $totalMass > 0 ? ($carbs / $totalMass) * 100 : 0;
+
+        $totalCal = ($proteins * 4) + ($fats * 9) + ($carbs * 4);
+
+        $proteinCalPercentage = $totalCal > 0 ? (($proteins * 4) / $totalCal) * 100 : 0;
+        $fatCalPercentage = $totalCal > 0 ? (($fats * 9) / $totalCal) * 100 : 0;
+        $carbCalPercentage = $totalCal > 0 ? (($carbs * 4) / $totalCal) * 100 : 0;
+
+        return [
+            'proteins' => $proteins,
+            'fats' => $fats,
+            'carbs' => $carbs,
+            'proteinCalPercentage' => $proteinCalPercentage,
+            'fatCalPercentage' => $fatCalPercentage,
+            'carbCalPercentage' => $carbCalPercentage
+        ];
+    }
+
     /**
      * Build the DataTable class.
      *
@@ -25,9 +69,33 @@ class RecipeDataTable extends DataTable
             ->addColumn('name', function(Recipe $recipe) {
                 return '<span>' . $recipe->name . '</span><img height="80" src="' . asset('storage/featured_recipes/' . $recipe->featured_image) . '" alt=""/>';
             })
+            ->addColumn('proteins', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['proteins'], 2) . 'g';
+            })
+            ->addColumn('fats', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['fats'], 2) . 'g';
+            })
+            ->addColumn('carbs', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['carbs'], 2) . 'g';
+            })
+            ->addColumn('proteins_percentage', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['proteinCalPercentage'], 2) . '%';
+            })
+            ->addColumn('fats_percentage', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['fatCalPercentage'], 2) . '%';
+            })
+            ->addColumn('carbs_percentage', function(Recipe $recipe) {
+                $macros = $this->getRecipeData($recipe);
+                return number_format($macros['carbCalPercentage'], 2) . '%';
+            })
             ->addColumn('type', function(Recipe $recipe) {
                 return match ($recipe->type) {
-                    2 => 'Ručak',
+                    2 => 'Ručak, Večera',
                     3 => 'Užina',
                     default => 'Doručak',
                 };
@@ -45,7 +113,7 @@ class RecipeDataTable extends DataTable
                 </form>
             ';
             })
-            ->rawColumns(['action', 'name'])
+            ->rawColumns(['action', 'name', 'macros'])
             ->setRowId('id');
     }
 
@@ -56,11 +124,14 @@ class RecipeDataTable extends DataTable
     {
         return $model->newQuery()
             ->select('recipes.*')
-            ->when(request('search')['value'], function ($query) {
-                $search = request('search')['value'];
+            ->when(request('search.value'), function ($query, $search) {
                 $query->where('name', 'like', "%{$search}%");
+            })
+            ->when(request('recipeType'), function ($query, $type) {
+                $query->where('type', $type);
             });
     }
+
 
     /**
      * Optional method if you want to use the html builder.
@@ -68,21 +139,26 @@ class RecipeDataTable extends DataTable
     public function html(): HtmlBuilder
     {
         return $this->builder()
-                    ->setTableId('recipe-table')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    //->dom('Bfrtip')
-                    ->orderBy(1)
-                    ->selectStyleSingle()
-                    ->buttons([
-                        Button::make('excel'),
-                        Button::make('csv'),
-                        Button::make('pdf'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload')
-                    ]);
+            ->setTableId('recipe-table')
+            ->columns($this->getColumns())
+            ->ajax([
+                'url' => route('show-recipes-list'),
+                'data' => 'function(d) {
+                    d.recipeType = $("#recipeType").val();
+                }'
+            ])
+            ->orderBy(1)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
     }
+
 
     /**
      * Get the dataTable columns definition.
@@ -91,6 +167,12 @@ class RecipeDataTable extends DataTable
     {
         return [
             Column::make('name')->title('Recept')->searchable(true),
+            Column::make('proteins')->title('Proteini')->searchable(false),
+            Column::make('fats')->title('Masti')->searchable(false),
+            Column::make('carbs')->title('Ugljeni hidrati')->searchable(false),
+            Column::make('proteins_percentage')->title('Proteini %')->searchable(false),
+            Column::make('fats_percentage')->title('Masti %')->searchable(false),
+            Column::make('carbs_percentage')->title('Ugljeni hidrati %')->searchable(false),
             Column::make('type')->title('Tip obroka'),
             Column::computed('action')
                 ->title('Akcije')
--- resources/views/recipes-list.blade.php
+++ resources/views/recipes-list.blade.php
@@ -22,15 +22,21 @@
 
 @section('content')
     <div class="admin-dashboard container mt-4">
-        <div class="row mb-4">
-            <div class="col-md-12">
-                <h2 class="text-center">Recepti</h2>
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="recipeType">Filtriraj po tipu obroka:</label>
+                <select id="recipeType" class="form-control">
+                    <option value="">Svi tipovi</option>
+                    <option value="1">Doručak</option>
+                    <option value="2">Ručak, Večera</option>
+                    <option value="3">Užina</option>
+                </select>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="table-responsive">
-                    {{ $dataTable->table(['class' => 'table table-striped table-bordered']) }}
+                    {{ $dataTable->table(['class' => 'table table-striped table-bordered', 'id' => 'recipe-table']) }}
                 </div>
             </div>
         </div>
@@ -39,4 +45,14 @@
 
 @section('scriptsBottom')
     {{ $dataTable->scripts() }}
+    <script>
+        $(document).ready(function () {
+            const table = $('#recipe-table').DataTable();
+
+            $('#recipeType').on('change', function () {
+                table.ajax.reload();
+            });
+        });
+    </script>
 @endsection
+
