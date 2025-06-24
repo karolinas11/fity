87106fc2ec33b025944bb311f33c79b3da84036a
Janko Blagojevic <janko.tbbt@gmail.com>
2025-06-24 12:22:49 +0200

    get recipe

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -317,18 +317,18 @@ class UserController extends Controller
     }
 
     public function getRecipeByUserIdAndRecipeId(Request $request) {
-        $firebaseUid = $this->authService->verifyUserAndGetUid($request->header('Authorization'));
-        if(!$firebaseUid) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+//        $firebaseUid = $this->authService->verifyUserAndGetUid($request->header('Authorization'));
+//        if(!$firebaseUid) {
+//            return response()->json(['error' => 'Unauthorized'], 401);
+//        }
         $recipe = $this->userRecipeService->getUserRecipeByUserIdAndRecipeId($request->input('recipeId'), $request->input('screen'));
-	if ($recipe->bookmark_status == 1) {
-		$recipe->bookmark_status = 'bookmarked';
-	} else if ($recipe->bookmark_status == -1) {
-                $recipe->bookmark_status = 'deleted';
-	} else {
-                $recipe->bookmark_status = 'active';
-	}
+        if ($recipe->bookmark_status == 1) {
+            $recipe->bookmark_status = 'bookmarked';
+        } else if ($recipe->bookmark_status == -1) {
+            $recipe->bookmark_status = 'deleted';
+        } else {
+            $recipe->bookmark_status = 'active';
+        }
         return response()->json($recipe);
     }
 
