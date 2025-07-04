b06de23de84378e1b9e43846d9701834d3ca87dc
Janko Blagojevic <janko.tbbt@gmail.com>
2025-07-04 14:11:08 +0200

    revert

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -701,39 +701,7 @@ class UserController extends Controller
             ->first();
         $userRecipe->status = $request->input('status');
         $userRecipe->save();
-        $recipe = UserRecipe::where('user_id', $user->id)
-            ->where('recipe_id', '!=', $request->input('recipeId'))
-            ->get()
-            ->first();
 
-        $r = Recipe::where('id', $recipe->recipe_id)->first();
-        $recipe->foodstuffs = $recipe->foodstuffs;
-        $recipe->type = $r->type;
-        $recipe->name = $r->name;
-        $recipe->preparation_time = $r->preparation_time;
-        $recipe->featured_image = $r->featured_image;
-        $description = str_replace('\n', "\n", $r->description);
-        $recipe->steps = preg_split('/\r\n|\r|\n/', $description);
-        $recipe->steps = array_filter($recipe->steps, fn($step) => trim($step) !== '');
-        $cal = 0;
-        $prot = 0;
-        $fat = 0;
-        $ch = 0;
-        foreach ($recipe->foodstuffs as &$foodstuff) {
-            $f = Foodstuff::where('id', $foodstuff->foodstuff_id)->get()[0];
-            $cal += $foodstuff->amount * ($f->calories / 100);
-            $prot += $foodstuff->amount * ($f->proteins / 100);
-            $fat += $foodstuff->amount * ($f->fats / 100);
-            $ch += $foodstuff->amount * ($f->carbohydrates / 100);
-            $foodstuff->foodstuff_category = FoodstuffCategory::where('id', $f->foodstuff_category_id)->get()[0]->name;
-            $foodstuff->name = $f->name;
-        }
-
-        $recipe->calAmount = $cal;
-        $recipe->proteinAmount = $prot;
-        $recipe->fatsAmount = $fat;
-        $recipe->chAmount = $ch;
-
-        return response()->json($recipe);
+        return response()->json($userRecipe);
     }
 }
